a271dc678d8ae7b9bac47fd6c7c1e9e932e7c5e1
Sudip B <realsudipbajgai@gmail.com>
2026-06-11 11:50:52 -0500

    Used signal in users list

--- inventory.client/src/app/features/users/pages/user-list/users.component.ts
+++ inventory.client/src/app/features/users/pages/user-list/users.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, Router } from '@angular/router'
 import { CommonModule } from '@angular/common';
-import { Observable, BehaviorSubject, Subject, startWith, switchMap } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { UserService } from '../../services/user.service';
 import { User } from '../../../../shared/models/User';
 import { ToastService } from '../../../../shared/services/toast.service';
@@ -13,38 +13,51 @@ import { ToastService } from '../../../../shared/services/toast.service';
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
-  constructor(private userService: UserService,
-    private router: Router,
-    private toastService: ToastService) { }
-  selectedUser: any = null;
-  private refresh$ = new Subject<void>();
-  users$ = this.refresh$.pipe(
-    startWith(void 0),
-    switchMap(() => this.userService.getAllUsers())
-  );
+  private userService = inject(UserService);
+  private toastService = inject(ToastService);
+  users = signal<any[]>([]);
+  selectedUser = signal<any>(null);
+  async ngOnInit() {
+    try {
+      const data = await firstValueFrom(this.userService.getAllUsers());
+      this.users.set(data);
+    }
+    catch (error) {
+      console.error('failed to load users', error);
+    }
+  }
+  selectUser(user: any) {
+    this.selectedUser.set(user);
+  }
+  async seedTestUsers() {
+    try {
+      await firstValueFrom(this.userService.seedTestUsers());
+      this.toastService.show('success', "Test users seeded successfully");
+      this.loadUsers();
+    }
+    catch (error) {
+      this.toastService.show('error', "Failed to seed data");
+    }
+  }
   onUserDetailsClick(user: User) {
-    this.selectedUser = user;
+    this.selectUser(user);
   }
   onUserDeleteClick(user: User) {
-    this.selectedUser = user;
+    this.selectUser(user);
   }
-  onDeleteConfirmation(id: number) {
-    this.userService.deleteUser(id).subscribe(data => {
-      if (data.success) {
-          console.log('NEW TOAST EVENT', data);
-        this.toastService.show('warning', data.message);
-      }
-      this.selectedUser = null;
-      this.refresh$.next();
-    });
+
+  async onDeleteConfirmation(id: number) {
+    try {
+      await firstValueFrom(this.userService.deleteUser(id));
+      this.loadUsers();
+      this.toastService.show('success', 'Delete Successful');
+    }
+    catch {
+      this.toastService.show('error', 'Delete Failed');
+    }
   }
-  seedTestUsers() {
-    this.userService.seedTestUsers().subscribe(data => {
-      if (data.success) {
-          console.log('NEW TOAST EVENT', data);
-        this.toastService.show('info', data.message);
-      }
-      this.refresh$.next();
-    });
+  async loadUsers() {
+    const updatedUsers = await firstValueFrom(this.userService.getAllUsers());
+    this.users.set(updatedUsers);
   }
 }
